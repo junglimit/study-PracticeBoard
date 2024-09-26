4a11c1f19d7fa8ed27e472c8d6a9fcadac6cbb1d
junglimit <jaehan_jjh@naver.com>
2024-09-26 14:08:40 +0900

    기본틀, DB전송

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -1,16 +1,28 @@
 package com.study.board.controller;
 
+import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class BoardController {
 
-    @GetMapping("/")
-    @ResponseBody
-    public String main() {
-        
-        return "Hello World!";
+    private final BoardService boardService;
+
+    @GetMapping("/board/write")
+    public String boardWriteForm() {
+
+        return "boardwrite";
+    }
+
+    @PostMapping("/board/writepro")
+    public String boardWritePro(Board board) {
+
+        boardService.write(board);
+        return "";
     }
 }
