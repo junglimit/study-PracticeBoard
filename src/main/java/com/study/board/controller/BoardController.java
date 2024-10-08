338f239d92a831c26017e8ce626679ca11ccdb00
junglimit <jaehan_jjh@naver.com>
2024-10-08 16:59:15 +0900

    리스트처리 상세페이지 처리

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -34,4 +34,12 @@ public class BoardController {
 
         return "boardlist";
     }
+
+    @GetMapping("/board/view")
+    public String boardView(Model model, Integer id){
+
+        model.addAttribute("board", boardService.boardView(id));
+
+        return "boardview";
+    }
 }
--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -13,13 +13,21 @@ public class BoardService {
 
     private final BoardRepository boardRepository;
 
+    // 글 작성 처리
     public void write(Board board) {
 
         boardRepository.save(board);
     }
 
+    // 게시글 리스트 가져오기
     public List<Board> boardList() {
 
         return boardRepository.findAll();
+    }
+
+    // 특정 게시글 가져오기
+    public Board boardView(Integer id) {
+
+        return boardRepository.findById(id).get();
     }
 }
